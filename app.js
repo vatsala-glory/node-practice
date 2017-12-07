1d368ec0a391cd6229def0bf98580703206fcd49
Vatsala Agrawal <vagr10@sapient.com>
2017-12-07 12:41:30 +0530

    added express with hbs

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.render('index.hbs');
+});
+
+module.exports = app;
